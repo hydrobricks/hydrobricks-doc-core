fc58b06d3fa66f248453bf98187b4a8fee29259b
pascalhorton <pascalhorton@users.noreply.github.com>
2025-05-07 15:10:48 +0000

    deploy: hydrobricks/hydrobricks@af83ca7f82c6dce28af16a5a70b5a0d569f88f4f

---
+++ classBrick.js
@@ -0,0 +1,8 @@
+var classBrick =
+[
+    [ "AttachFluxIn", "classBrick.html#a996239fbc8712128d351770d6a33a15b", null ],
+    [ "GetDynamicContentChanges", "classBrick.html#ae41fca75530e2efccdaeee664060faef", null ],
+    [ "IsOk", "classBrick.html#a165dd5936c95d9f104c16a650f7601fe", null ],
+    [ "NeedsSolver", "classBrick.html#a86d8a717a3e4771b0605492bf7291ee3", null ],
+    [ "SetParameters", "classBrick.html#a87b7d1abdcc3614f2baf44ba6b6b5daf", null ]
+];
---
+++ classUrban.js
@@ -0,0 +1,4 @@
+var classUrban =
+[
+    [ "SetParameters", "classUrban.html#a008d7dc604992b4d4a77d1ae1277dc19", null ]
+];
